f86a9edfb3023675440a1434c3fb68197bb5bf3b
1144746221 <weiwang.ysu@gmail.com>
2020-05-17 15:58:21 +0800

    fix reference input to list of list

--- nlgeval/__init__.py
+++ nlgeval/__init__.py
@@ -286,7 +286,8 @@ class NLGEval(object):
         return ret_scores
 
     def compute_metrics(self, ref_list, hyp_list):
-        ref_list = [list(map(_strip, refs)) for refs in zip(*ref_list)]
+        #ref_list = [list(map(_strip, refs)) for refs in zip(*ref_list)]
+        ref_list = [list(map(_strip, refs)) for refs in ref_list]
         refs = {idx: strippedlines for (idx, strippedlines) in enumerate(ref_list)}
         hyps = {idx: [lines.strip()] for (idx, lines) in enumerate(hyp_list)}
         assert len(refs) == len(hyps)
